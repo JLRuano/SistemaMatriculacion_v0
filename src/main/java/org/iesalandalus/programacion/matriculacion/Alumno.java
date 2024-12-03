dae35cfc155e53fc8ba68a71f8e27bcc39e20df2
JLRuano <j.l.ruano.clases@gmail.com>
2024-12-03 11:36:48 +0100

    paso 2 (crear atributos de alumno)

--- src/main/java/org/iesalandalus/programacion/matriculacion/Alumno.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/Alumno.java
@@ -2,4 +2,13 @@ package org.iesalandalus.programacion.matriculacion;
 
 public class Alumno {
 
+    //paso 1 crea los atributos
+    private static String ER_TELEFONO;
+    private static String ER_CORREO;
+    private static String ER_DNI;
+
+    public static String FORMATO_FECHA = "dd/MM/YYYY";
+    private static String ER_NIA;
+    private static int MIN_EDAD_ALUMNADO;
+
 }
